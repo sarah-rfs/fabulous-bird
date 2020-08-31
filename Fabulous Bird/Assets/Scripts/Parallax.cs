eb762e429f5b52549189532651635de4dbd937ab
Sarah Rodrigues <sarahrfs91@gmail.com>
2020-08-31 20:41:27 -0300

    Chão agora mexe, só falta ser eterno

--- Fabulous Bird/Assets/Scripts/Parallax.cs
+++ Fabulous Bird/Assets/Scripts/Parallax.cs
@@ -4,27 +4,17 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    private float length;
-    private float StartPos;
 
-    private Transform cam;
+    public float speed;
 
-    public float ParallaxEffect;
-    // Start is called before the first frame update
     void Start()
     {
-        StartPos = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
-        cam = Camera.main.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float RePos = cam.transform.position.x* (1 - ParallaxEffect);
-        float Distance = cam.transform.position.x * ParallaxEffect;
-
-        transform.position = new Vector3(StartPos + Distance, transform.position.y, transform.position.z);
+        transform.position += Vector3.left * speed * Time.deltaTime;
         
     }
 }
