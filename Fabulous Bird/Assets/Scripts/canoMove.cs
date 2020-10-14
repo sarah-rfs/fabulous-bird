87aa606f5199707f2a86b83a7b1e7d57c6f7bb3d
Sarah Rodrigues <sarahrfs91@gmail.com>
2020-10-14 10:05:56 -0300

    Adicionando canos

---
+++ Fabulous Bird/Assets/Scripts/canoMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class canoMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+ void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+        if (transform.position.x <= -4.8f)
+        {
+            transform.position = new Vector3(4.6f,transform.position.y, transform.position.z);
+        }
+    }
+}
